39bddc622f0731ed418c58e4b12598242309f368
ismailAbdirahman <ismail960i960@gmail.com>
2023-02-20 22:41:48 +0300

    added viewing user posts from profile

--- lib/features/home/perserntation/home_screen.dart
+++ lib/features/home/perserntation/home_screen.dart
@@ -10,6 +10,7 @@ import 'package:riverpod_annotation/riverpod_annotation.dart';
 import '../../profile/controller/view_profile_controller.dart';
 import '../../profile/data/profile_service.dart';
 import '../../profile/domain/profileModel.dart';
+import '../../../shared_widgets/post_tile.dart';
 import '../../profile/persentation/shared_widegts/view_profile_tile.dart';
 import '../controller/home_controller.dart';
 import '../service/home_service.dart';
@@ -21,8 +22,6 @@ class HomeScreen extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final homeRef = ref.watch(homeControllerProvider);
     final myID = ref.watch(viewControllerProvider).value;
-
-
 
     return Scaffold(
         appBar: AppBar(
@@ -55,143 +54,20 @@ class HomeScreen extends ConsumerWidget {
                                     .read(homeControllerProvider.notifier)
                                     .likePost(data[index].id);
                               },
-                              child: PostTile(homeModel: data[index]));
+                              child: PostTile(
+                                username: data[index].author.username,
+                                userID: data[index].author.id,
+                                profileImage: data[index].author.image,
+                                postId: data[index].id,
+                                postImage: data[index].image,
+                                likes: data[index].likes,
+                                caption: data[index].caption,
+                              ));
                         },
                       ),
                   error: (e, r) => Text(e.toString()),
                   loading: () =>
                       const Center(child: CircularProgressIndicator())))
         ]));
-  }
-}
-
-class PostTile extends ConsumerWidget {
-  PostTile({super.key, required this.homeModel});
-
-  final HomeModel homeModel;
-  late bool isLiked;
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    return Padding(
-      padding: const EdgeInsets.all(3.0),
-      child: Card(
-        elevation: 0,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                children: [
-                  SizedBox(
-                    height: 50,
-                    width: 50,
-                    child: Image(
-                      image: NetworkImage(homeModel.author.image),
-                      fit: BoxFit.fill,
-                    ),
-                  ),
-                  InkWell(
-                    onTap: () async {
-                      final result = await ref
-                          .read(viewControllerProvider.notifier)
-                          .viewUserProfile(homeModel.author.id);
-
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) =>
-                                ViewProfileTile(data: result)),
-                      );
-                    },
-                    child: Padding(
-                      padding: const EdgeInsets.only(left: 8.0),
-                      child: Text(
-                        homeModel.author.username,
-                        style: const TextStyle(
-                            fontWeight: FontWeight.w700,
-                            color: Color(0xFF306088)),
-                      ),
-                    ),
-                  )
-                ],
-              ),
-            ),
-            SizedBox(
-              height: 400,
-              width: double.infinity,
-              child: Image(
-                image: NetworkImage(homeModel.image),
-                fit: BoxFit.fitWidth,
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 8, left: 0.0),
-              child: Row(
-                children: [
-                  homeModel.likes.contains("63e29eead6c3e3f9a436c13d") == true
-                      ? IconButton(
-                          onPressed: () {
-                            ref
-                                .read(homeControllerProvider.notifier)
-                                .likePost(homeModel.id);
-                          },
-                          icon: const Icon(
-                            Icons.favorite,
-                            color: Colors.redAccent,
-                          ),
-                        )
-                      : IconButton(
-                          onPressed: () {
-                            print("-0-0-0-0-0 ${homeModel.id}-0-0-00-");
-                            ref
-                                .read(homeControllerProvider.notifier)
-                                .likePost(homeModel.id);
-                          },
-                          icon: const Icon(
-                            Icons.favorite_border,
-                            color: Colors.redAccent,
-                          ),
-                        ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 4.0),
-                    child: Text(
-                      '${homeModel.likes.length.toString()} likes',
-                      style: const TextStyle(
-                          fontWeight: FontWeight.w700,
-                          color: Color(0xFF306088)),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 6.0),
-              child: Wrap(
-                //  crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Icon(
-                    Icons.comment_bank,
-                    size: 16,
-                    color: Colors.grey,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(right: 8.0),
-                    child: Text(
-                      homeModel.author.username,
-                      style: const TextStyle(
-                          fontWeight: FontWeight.w700,
-                          color: Color(0xFF306088)),
-                    ),
-                  ),
-                  Text(homeModel.caption)
-                ],
-              ),
-            )
-          ],
-        ),
-      ),
-    );
   }
 }
---
+++ lib/shared_widgets/post_tile.dart
@@ -0,0 +1,150 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:oldinsa/features/profile/persentation/shared_widegts/view_profile_tile.dart';
+
+import '../features/home/controller/home_controller.dart';
+import '../features/profile/controller/view_profile_controller.dart';
+
+class PostTile extends ConsumerWidget {
+  PostTile(
+      {super.key,
+        required this.username,
+        required this.caption,
+        required this.postImage,
+        required this.likes,
+        required this.profileImage,
+        required this.userID,
+        required this.postId});
+
+  final String userID;
+  final String postId;
+  final String username;
+  final String profileImage;
+  final String postImage;
+  final String caption;
+  final List likes;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Padding(
+      padding: const EdgeInsets.all(3.0),
+      child: Card(
+        elevation: 0,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  SizedBox(
+                    height: 50,
+                    width: 50,
+                    child: Image(
+                      image: NetworkImage(profileImage),
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                  InkWell(
+                    onTap: () async {
+                      final result = await ref
+                          .read(viewControllerProvider.notifier)
+                          .viewUserProfile(userID);
+
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) =>
+                                ViewProfileTile(data: result)),
+                      );
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 8.0),
+                      child: Text(
+                        username,
+                        style: const TextStyle(
+                            fontWeight: FontWeight.w700,
+                            color: Color(0xFF306088)),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 400,
+              width: double.infinity,
+              child: Image(
+                image: NetworkImage(postImage),
+                fit: BoxFit.fitWidth,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 8, left: 0.0),
+              child: Row(
+                children: [
+                  likes.contains("63e29eead6c3e3f9a436c13d") == true
+                      ? IconButton(
+                    onPressed: () {
+                      ref
+                          .read(homeControllerProvider.notifier)
+                          .likePost(userID);
+                    },
+                    icon: const Icon(
+                      Icons.favorite,
+                      color: Colors.redAccent,
+                    ),
+                  )
+                      : IconButton(
+                    onPressed: () {
+                      ref
+                          .read(homeControllerProvider.notifier)
+                          .likePost(userID);
+                    },
+                    icon: const Icon(
+                      Icons.favorite_border,
+                      color: Colors.redAccent,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 4.0),
+                    child: Text(
+                      '${likes.length.toString()} likes',
+                      style: const TextStyle(
+                          fontWeight: FontWeight.w700,
+                          color: Color(0xFF306088)),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 6.0),
+              child: Wrap(
+                //  crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Icon(
+                    Icons.comment_bank,
+                    size: 16,
+                    color: Colors.grey,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 8.0),
+                    child: Text(
+                      username,
+                      style: const TextStyle(
+                          fontWeight: FontWeight.w700,
+                          color: Color(0xFF306088)),
+                    ),
+                  ),
+                  Text(caption)
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
